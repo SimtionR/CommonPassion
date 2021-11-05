bc1cdb831aff3a43b664734e6572a167124b4bf4
SimtionR <simtionrazvan7@gmail.com>
2021-11-05 17:01:08 +0200

    Proeict initial

---
+++ CommonPassion_Backend/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CommonPassion_Backend.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System.Diagnostics;
+
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [Authorize]
+        public IActionResult Get()
+        {
+
+            return Ok();
+        }
+        
+    }
+}
